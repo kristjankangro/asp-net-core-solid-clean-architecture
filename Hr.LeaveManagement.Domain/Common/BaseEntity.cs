ed2374a88eca468753490d4dcd82c0a57ae5fb7c
kristjan kangro <kristjan.kangro@visma.com>
2025-04-26 14:51:35 +0300

    init

---
+++ Hr.LeaveManagement.Domain/Common/BaseEntity.cs
@@ -0,0 +1,8 @@
+namespace Hr.LeaveManagement.Domain.Common;
+
+public abstract class BaseEntity
+{
+	public int Id { get; set; }
+	public DateTime? CreatedDate { get; set; } = DateTime.Now;
+	public DateTime? ModifiedDate { get; set; } = DateTime.Now;
+}
