59c713351248a1b825edaf932d050d60063d98a9
kristjan kangro <kristjan.kangro@visma.com>
2025-05-29 09:51:23 +0300

    enxt /review

--- Hr.LeaveManagement.App/Features/LeaveType/Queries/GetAllLeaveTypes/LeaveType2Dto.cs
+++ Hr.LeaveManagement.App/Features/LeaveType/Queries/GetAllLeaveTypes/LeaveType2Dto.cs
@@ -1,7 +1,6 @@
 namespace Hr.LeaveManagement.App.Features.LeaveType.Queries.GetAllLeaveTypes;
 
-public class LeaveType2Dto : LeaveTypeDto
+public class LeaveTypeForSpecialNeedsDto : LeaveTypeDto
 {
-	public int Id { get; set; }
 	public int DefaultDays2 { get; set; }
 }
