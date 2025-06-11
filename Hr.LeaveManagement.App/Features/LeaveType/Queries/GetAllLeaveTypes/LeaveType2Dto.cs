e14e4b6e5e7e8befc77702c537f217dd5f85d3c9
kristjan kangro <kristjan.kangro@visma.com>
2025-06-11 19:16:15 +0300

    nn

---
+++ Hr.LeaveManagement.App/Features/LeaveType/Queries/GetAllLeaveTypes/LeaveType2Dto.cs
@@ -0,0 +1,6 @@
+namespace Hr.LeaveManagement.App.Features.LeaveType.Queries.GetAllLeaveTypes;
+
+public class LeaveType2Dto: LeaveTypeDto
+{
+	public string Description { get; set; } = string.Empty;
+}
--- Hr.LeaveManagement.App/Features/LeaveType/Queries/GetAllLeaveTypes/LeaveTypeDto.cs
+++ Hr.LeaveManagement.App/Features/LeaveType/Queries/GetAllLeaveTypes/LeaveTypeDto.cs
@@ -6,8 +6,3 @@ public class LeaveTypeDto
 	public string Name { get; set; } = string.Empty;
 	public int DefaultDays { get; set; }
 }
-
-public class LeaveType2Dto: LeaveTypeDto
-{
-	public string Description { get; set; } = string.Empty;
-}
