0c4dc49540d7e96f3babbff26ae61447c9680c37
kristjan kangro <kristjan.kangro@visma.com>
2025-06-11 19:13:42 +0300

    add LeaveType2Dto to extend LeaveTypeDto

--- Hr.LeaveManagement.App/Features/LeaveType/Queries/GetAllLeaveTypes/LeaveTypeDto.cs
+++ Hr.LeaveManagement.App/Features/LeaveType/Queries/GetAllLeaveTypes/LeaveTypeDto.cs
@@ -6,3 +6,8 @@ public class LeaveTypeDto
 	public string Name { get; set; } = string.Empty;
 	public int DefaultDays { get; set; }
 }
+
+public class LeaveType2Dto: LeaveTypeDto
+{
+	public string Description { get; set; } = string.Empty;
+}
