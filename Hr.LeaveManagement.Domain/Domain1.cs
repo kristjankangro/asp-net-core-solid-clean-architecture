e19e52a2cc5dc142ae9238f2cd810da23474ffd3
Kristjan <kristjan.kangro@visma.com>
2025-06-27 12:13:56 +0300

    main 0.1

--- Hr.LeaveManagement.Domain/Domain1.cs
+++ Hr.LeaveManagement.Domain/Domain1.cs
@@ -14,3 +14,13 @@ interface IDomain1
 {
 	string Email { get; set; }
 } 
+
+interface IDomain2
+{
+	string Name { get; set; }
+}
+
+public class Domain3 : IDomain2
+{
+	public string Name { get; set; }
+}
