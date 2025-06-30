a2d2ba50f6c9ee616eaeb88ee10c831ffddc8ef9
kristjan kangro <kristjan.kangro@visma.com>
2025-06-30 17:38:49 +0300

    refactor: clean up unused imports and adjust namespace in DataProcessorService

---
+++ Hr.LeaveManagement.Domain/test.cs
@@ -0,0 +1,10 @@
+namespace Hr.LeaveManagement.Domain;
+
+/// <summary>
+/// test class fro transaction either deibt or credit
+/// </summary>
+public class test
+{
+	public bool IsCredit { get; set; } 
+	public bool IsDebit { get; set; }
+}
