663acb63e2a36808bd8b2d0f87a64996f3ef11a8
kristjan kangro <kristjan.kangro@visma.com>
2025-06-30 17:47:46 +0300

    refactor: rename test class to Test22 in Hr.LeaveManagement.Domain

--- Hr.LeaveManagement.Domain/test.cs
+++ Hr.LeaveManagement.Domain/test.cs
@@ -3,7 +3,7 @@ namespace Hr.LeaveManagement.Domain;
 /// <summary>
 /// test class fro transaction either deibt or credit
 /// </summary>
-public class test
+public class Test22
 {
 	public bool IsCredit { get; set; } 
 	public bool IsDebit { get; set; }
