4a90674a40891f7d13df2ffee563e30edf23c378
Kristjan Kangro <87314450+kristjankangro@users.noreply.github.com>
2025-06-30 17:51:09 +0300

    Merge pull request #12
    * refactor: clean up unused imports and adjust namespace in DataProcess…
    
    * refactor: rename test class to Test22 in Hr.LeaveManagement.Domain

---
+++ Hr.LeaveManagement.Domain/test.cs
@@ -0,0 +1,10 @@
+namespace Hr.LeaveManagement.Domain;
+
+/// <summary>
+/// test class fro transaction either deibt or credit
+/// </summary>
+public class Test22
+{
+	public bool IsCredit { get; set; } 
+	public bool IsDebit { get; set; }
+}
